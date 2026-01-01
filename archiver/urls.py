8fe61972dad5294e8d56108f9b87ac20347a0bd4
pc777 <inu9431@naver.com>
2026-01-02 01:14:13 +0900

    ♻️ (refactor): 뷰를 등록하도록 url 수정

--- archiver/urls.py
+++ archiver/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'archiver'  # 템플릿에서 주소를 편하게 쓰기 위한 별칭
 
 urlpatterns = [
-    path('/', views.QnABotAPIView.as_view(), name='qna_bot'),  # http://127.0.0.1:8000/archiver/ 주소
+    path('qna/', views.QnABotAPIView.as_view(), name='qna_bot'),  # http://127.0.0.1:8000/archiver/ 주소
 
 ]
