34e6a6cee0e7e336409c31936659fc8e38a6f5d1
inu9431 <>
2026-01-12 14:36:05 +0900

    최종

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 @pytest.mark.django_db
 def test_user_creation():
     # 장고 DB가 정상적으로 연결되어 유저가 생성되는지 확인하는 기초 테스트
-user = User.objects.create_user(username="testuser", password="password")
-assert user.username ==  "testuser"
-assert User.objects.count() == 1
+    user = User.objects.create_user(username="testuser", password="password")
+    assert user.username ==  "testuser"
+    assert User.objects.count() == 1
