ef310b345913d49bc07827ea3e4c75551505dcd4
inu9431 <inu9431@naver.com>
2026-01-12 21:39:05 +0900

    health url 추가

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,16 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
+
+def health_check(request):
+    return HttpResponse("ok", status=200)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("archiver/", include("archiver.urls")),
+    path('health/', health_check),
 ]
 
 if settings.DEBUG:
