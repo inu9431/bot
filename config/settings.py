7ba2ced65335f9fadcf3f43dfb8de5de58fc6c4d
inu9431 <>
2026-01-12 15:48:45 +0900

    settings 수정

--- config/settings.py
+++ config/settings.py
@@ -137,16 +137,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": env("POSTGRES_DB"),
-        "USER": env("POSTGRES_USER"),
-        "PASSWORD": env("POSTGRES_PASSWORD"),
-        "HOST": env("POSTGRES_HOST", default="db"),
-        "PORT": env("POSTGRES_PORT", default="5432"),
-    }
+    'default' : env.db(
+        'DATABASE_URL',
+        default='postgres://bot_user:bot_password@localhost:5432/bot_db'
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
