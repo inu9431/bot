471096a2e1171e8d4ae7aaa7827b41407732fdd9
inu9431 <>
2026-01-12 18:04:45 +0900

    log수정

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import environ
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-LOG_DIR = os.path.join(BASE_DIR, "logs")
+LOG_DIR = os.path.join(BASE_DIR, "logs", exist_ok=True)
 if not os.path.exists(LOG_DIR):
     os.makedirs(LOG_DIR)
 
