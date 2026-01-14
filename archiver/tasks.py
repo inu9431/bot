4e6e9bd294e8bcfad2ec142326171f3caae9175c
inu9431 <>
2026-01-14 14:46:42 +0900

    fix: 기존의 ai분석 업무를 종료하고 관리자가 승인한 데이터를 노션api로 전송하는 비동기 업무만 전담하도록 수정 notionadapter 와 qnadto를 활용해 외부 api통신을 안정적으로 처리하도록 수정

--- archiver/tasks.py
+++ archiver/tasks.py
@@ -1,37 +1,42 @@
 import logging
 import os
-
 import requests
-
-from .services import QnAService
-
+from .adapters import NotionAdapter
+from .models import QnALog
+from django_q.tasks import async_task
+from .dto import QnADTO
 logger = logging.getLogger(__name__)
 
 
-def task_process_question(question_text: str, image_path: str = None):
-    service = QnAService()
-
-    if image_path and not os.path.exists(image_path):
-        logger.warning(f"태스크 시작 전 이미지 파일이 존재하지 않음: {image_path}")
-
+def task_process_question(log_id):
+    """
+    worker에서 실행되는 비동기 태스크
+    """
     try:
-        # 1. AI 분석 및 DB 저장
-        obj, is_duplicated = service.process_question_flow(question_text, image_path)
+        # DB에서 가져오기
+        log = QnALog.objects.get(id=log_id)
+        # adapter가 이해할수 있는 dto로 변환
+        dto = QnADTO(
+            title=log.title,
+            question_text=log.question_text,
+            ai_answer=log.ai_answer,
+            category=log.category,
+            hit_count=log.hit_count,
+            keywords=log.keywords,
+        )
+        # adapter 호출
+        adapter = NotionAdapter()
+        notion_url = adapter.create_qna_page(dto)
 
-        # 2. 웹훅 발송
-        webhook_url = os.getenv("DISCORD_WEBHOOK_URL")
-        if webhook_url:
-            payload = {
-                # 괄호와 문구를 정리했습니다.
-                "content": f"✅ **AI 분석 완료 (ID: {obj.id})**\n\n{obj.ai_answer}\n\n*관리자 검증 대기 중입니다. 어드민에서 확인해주세요.*"
-            }
-            # 실제 발송
-            res = requests.post(webhook_url, json=payload)
-            logger.info(f"디코드 웹훅 발송 결과 코드: {res.status_code}")
-
-        logger.info(f" QnA 처리완료: ID{obj.id} (중복여부: {is_duplicated})")
-        return f" 처리 완료 ID {obj.id} (중복: {is_duplicated})"
-
+        # 결과 저장
+        if notion_url:
+            log.notion_page_url = notion_url
+            log.save()
+            logger.info(f" [worker] 노션 업로드 완료 (ID: {log.id}")
     except Exception as e:
-        logger.error(f" 비동기 태스크 실패: {str(e)}", exc_info=True)
+        logger.error(f" [worker] 노션 업로드 실패 (ID: {log_id} : {str(e)}")
+        # 실패시 예외를 다시 던져서 django-q가  실패로그를 남김
         raise e
+
+
+
